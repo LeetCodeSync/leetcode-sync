45360acae5d6f79ebb015619291d5baab985ab83
Paul Shynin <pshynin@yahoo.com>
2026-04-09 10:07:48 -0400

    jest conf

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.test.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  clearMocks: true,
+  restoreMocks: true
+};
